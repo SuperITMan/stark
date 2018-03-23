4c0a77f0cf60b63c7fd47c8c475761e893b65a99
Alexis Georges <alexis.georges@nbb.be>
2018-03-23 12:06:02 +0100

    Move some variables

--- build.sh
+++ build.sh
@@ -12,13 +12,9 @@ set -u -e -o pipefail
 
 readonly currentDir=$(cd $(dirname $0); pwd)
 # TODO review
-source ${currentDir}/scripts/ci/_travis-fold.sh
-
-source ${currentDir}/build-functions.sh
+LOGS_DIR="reports"
 
 cd ${currentDir}
-
-LOGS_DIR="reports"
 
 # List of all packages, separated by a space
 # Packages will be transpiled using NGC (unless if also part of NODE_PACKAGES like the build package)
@@ -44,9 +40,20 @@ TRACE=false
 
 PROJECT_ROOT_DIR=`pwd`
 ROOT_DIR=${PROJECT_ROOT_DIR}/packages
-logTrace "Root dir: $ROOT_DIR"
 
 export NODE_PATH=${NODE_PATH:-}:${currentDir}/node_modules
+
+TSC=`pwd`/node_modules/.bin/tsc
+NGC="node --max-old-space-size=3000 `pwd`/node_modules/@angular/compiler-cli/src/main"
+UGLIFY=`pwd`/node_modules/.bin/uglifyjs
+TSCONFIG=./tools/tsconfig.json
+ROLLUP=`pwd`/node_modules/.bin/rollup
+
+source ${currentDir}/scripts/ci/_travis-fold.sh
+
+source ${currentDir}/build-functions.sh
+
+logTrace "Root dir: $ROOT_DIR"
 
 for ARG in "$@"; do
   case "$ARG" in
@@ -152,12 +159,7 @@ else
 fi
 logInfo "============================================="
 
-TSC=`pwd`/node_modules/.bin/tsc
 logTrace "TSC Path: $TSC"
-NGC="node --max-old-space-size=3000 `pwd`/node_modules/@angular/compiler-cli/src/main"
-UGLIFY=`pwd`/node_modules/.bin/uglifyjs
-TSCONFIG=./tools/tsconfig.json
-ROLLUP=`pwd`/node_modules/.bin/rollup
 
 if [[ ${BUILD_ALL} == true && ${TYPECHECK_ALL} == true ]]; then
   travisFoldStart "clean dist" "no-xtrace"
