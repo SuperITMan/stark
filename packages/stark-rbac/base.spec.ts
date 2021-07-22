0a5c7e531305ab434e8a34645f8b9d60be159a23
Alexis Georges <alexis@georges.brussels>
2021-07-22 16:41:07 +0200

    feat(stark-rbac): upgrade to Angular 12

--- packages/stark-rbac/base.spec.ts
+++ packages/stark-rbac/base.spec.ts
@@ -18,8 +18,8 @@ if ((<any>window).NodeList && !NodeList.prototype.forEach) {
 }
 
 /* tslint:disable:no-import-side-effect */
-import "zone.js/dist/zone";
-import "zone.js/dist/zone-testing";
+import "zone.js";
+import "zone.js/testing";
 import "zone.js/dist/long-stack-trace-zone";
 /* tslint:enable:no-import-side-effect */
 
